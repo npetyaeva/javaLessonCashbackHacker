426229ac9686953fc287c9b72cc711666dcc99bc
Natalia Petyaeva <npetyaeva@gmail.com>
2021-12-09 11:59:56 +0300

    Added tests

---
+++ src/test/java/ru/netology/CashbackHackerJunit4Test.java
@@ -0,0 +1,34 @@
+package ru.netology;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackerJunit4Test {
+    private final CashbackHacker service = new CashbackHacker();
+
+    @Test
+    public void shouldRemainLessBoundary() {
+        assertEquals(700, service.remain(300));
+    }
+
+    @Test
+    public void shouldRemainBoundaryMinusOne() {
+        assertEquals(1, service.remain(999));
+    }
+
+    @Test
+    public void shouldRemainBoundary() {
+        assertEquals(0, service.remain(1000));
+    }
+
+    @Test
+    public void shouldRemainBoundaryPlusOne() {
+        assertEquals(999, service.remain(1001));
+    }
+
+    @Test
+    public void shouldRemainMoreBoundary() {
+        assertEquals(495, service.remain(2505));
+    }
+}
--- src/test/java/ru/netology/CashbackHackerJupiterTest.java
+++ src/test/java/ru/netology/CashbackHackerJupiterTest.java
@@ -1,34 +1,34 @@
 package ru.netology;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CashbackHackerTest {
+class CashbackHackerJupiterTest {
     private final CashbackHacker service = new CashbackHacker();
 
     @Test
-    public void shouldRemainLessBoundary() {
+    void shouldRemainLessBoundary() {
         assertEquals(700, service.remain(300));
     }
 
     @Test
-    public void shouldRemainBoundaryMinusOne() {
+    void shouldRemainBoundaryMinusOne() {
         assertEquals(1, service.remain(999));
     }
 
     @Test
-    public void shouldRemainBoundary() {
+    void shouldRemainBoundary() {
         assertEquals(0, service.remain(1000));
     }
 
     @Test
-    public void shouldRemainBoundaryPlusOne() {
+    void shouldRemainBoundaryPlusOne() {
         assertEquals(999, service.remain(1001));
     }
 
     @Test
-    public void shouldRemainMoreBoundary() {
+    void shouldRemainMoreBoundary() {
         assertEquals(495, service.remain(2505));
     }
 }
